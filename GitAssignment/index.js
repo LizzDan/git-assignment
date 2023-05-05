bf3d0ecc053a0fbec7a390296360e372f5b98676
Otenaike Oluwatosin <125203838+LizzDan@users.noreply.github.com>
2023-05-05 14:07:00 +0100

    initial commit

---
+++ GitAssignment/index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn");
+
+function myFunc() {
+    alert("Hello World")
+}
+
+btn.addEventListener("click", myFunc)
+// var btn = document.getElementById.innerTex("btn")
